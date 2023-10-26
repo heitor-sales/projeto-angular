ca2eab4e81e7a2a0fa87e6132e9bf2da2fa7bd65
Heitor <salesheitor01@gmail.com>
2023-10-26 01:28:47 -0300

    last commit

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,12 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  export class AppComponent {
+    cartCount = 0;
+  
+    // Função para incrementar o número do carrinho
+    addToCart() {
+      this.cartCount++;
+    }
+  }
